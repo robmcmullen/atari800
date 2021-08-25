cfa9fdc15917a27430e2363b1f0eab4c1b14ca17
Rob McMullen <feedback@playermissile.com>
2021-08-25 12:05:30 -0700

    headless: expanded type definition to include commands like {step 5} or {step 5s} for 5 seconds worth

--- src/headless/globals.h
+++ src/headless/globals.h
@@ -17,16 +17,24 @@ extern int output_media_type;
 #define COMMAND_RECORD 1
 #define COMMAND_STEP 2
 #define COMMAND_TYPE 3
+#define COMMAND_KEYSTROKE 4
 
 typedef struct {
-    int command;
-    int number;
-} COMMAND_entry;
+    ULONG command;
+    SLONG number;
+    SLONG index;
+    UBYTE keychar;
+    UBYTE keycode;
+    UBYTE shift;
+    UBYTE control;
+    UBYTE modifier;
+} COMMAND_t;
 
 int GLOBALS_SetOutputFile(char *filename);
 int GLOBALS_AddIntCommand(int cmd, int arg);
 int GLOBALS_AddStrCommand(int cmd, char *arg);
 void GLOBALS_ShowCommands(void);
+void GLOBALS_ProcessCommand(COMMAND_t *cmd);
 void GLOBALS_RunCommands(void);
 
 #endif /* HEADLESS_GLOBALS_H_ */
